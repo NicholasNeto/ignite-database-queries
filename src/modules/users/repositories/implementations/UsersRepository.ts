1b61ed9a8fc1f4896e8d8b1ff071a90c4506bfd4
NicholasNeto <nicholasneto@outlook.com>
2022-03-06 10:50:22 -0300

    fix project

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,33 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      }
+    });
+
+    return user;
   }
 
+
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT * FROM users ORDER BY first_name ASC
+    `); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const user = await this.repository.query(`
+    SELECT email, first_name, last_name FROM users
+      WHERE LOWER(first_name) LIKE LOWER($1) 
+      AND LOWER(last_name) LIKE LOWER($2)
+  `, [first_name, last_name])
+  
+  return user 
   }
 }
